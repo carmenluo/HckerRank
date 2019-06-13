2a55d69c251b98231758bb551235e3990fa63c66
Carmen Luo <sdluojm@gmail.com>
2019-06-13 15:46:18 -0400

    Add files via upload

---
+++ ArrayLeftRotate/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ArrayLeftRotation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nd = Console.ReadLine().Split(" ");
+            int n = Convert.ToInt32(nd[0]);
+            int d = Convert.ToInt32(nd[1]);
+            int[] a = Array.ConvertAll(Console.ReadLine().Split(" "), atemp => Convert.ToInt32(atemp));
+            int[] result = rotateLeft(a, d);
+            Console.WriteLine(string.Join(" ", result));
+        }
+
+        private static int[] rotateLeft(int[] a, int d)
+        {
+            int[] temp = new int[a.Length];
+            int num = a.Length;
+            int _d = d % num;
+            for (int i = 0; i < num; i++)
+            {
+                temp[i] = a[i];
+            }
+            for (int i = d; i < num; i++)
+            {
+                a[i - d] = a[i];
+            }
+            for (int i = 0; i < d; i++)
+            {
+                a[num - d + i] = temp[i];
+            }
+            return a;
+        }
+    }
+}
